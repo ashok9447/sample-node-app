8b75b9211319e2fc3974c58a3ffd0af2f5f9f8b4
Ashok R <Ashok.R3@ibm.com>
2025-07-26 20:37:10 +0530

    Testing CI/CD webhook

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("🎉 Hello from OpenShift custom Node.js container!");
+  res.send("🎉 Hello from OpenShift custom Node.js containers!");
 });
 
 app.listen(PORT, () => {
